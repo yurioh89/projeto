d647047dfe49119fc261b851e5248ffc5b29d591
Yuri Santos <sk_yurioh89@hotmail.com>
2026-09-07 21:34:02 +0100

    html

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def _home(request):
-    return render(request, "home.html")
+    return render(request, "recipes/pages/home.html")
 
 def _sobre(request):
-    return render(request, "sobre.html")
+    return render(request, "recipes/pages/sobre.html")
 
 
 def _contato(request):
-    return render(request, "contato.html")
+    return render(request, "recipes/pages/contato.html")
